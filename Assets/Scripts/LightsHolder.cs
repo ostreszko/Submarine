ed570a2d8519cb370b87a5be8e2cdcbc12fd9b41
Paweł Horaczy <pawel.horaczy@bigcheesestudio.com>
2021-02-26 13:45:12 +0100

    Init

---
+++ Assets/Scripts/LightsHolder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class LightsHolder : MonoBehaviour
+{
+    [SerializeField]
+    private Transform spotLightTransform;
+    
+    [SerializeField]
+    private Transform playerTransform;
+    
+    void Update()
+    {
+        spotLightTransform.position =
+            new Vector3(playerTransform.position.x, playerTransform.position.y, spotLightTransform.position.z);
+    }
+}
---
+++ Assets/Scripts/SubmarineController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SubmarineController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody submarineRb;
+
+    [SerializeField] 
+    private float submarineSpeed;
+
+    [SerializeField] 
+    private GameObject propellor;
+    
+    [SerializeField] 
+    private float propellorRotationSpeed;
+
+    private float velocityX;
+    private float velocityY;
+    private float velocityZ;
+
+    private float horizontalAxis;
+    
+    private Vector3 leftRotation = new Vector3(0, 0, 0);
+    private Vector3 rightRotation = new Vector3(0, -180, 0);
+    
+    void Start()
+    {
+        
+    }
+    
+    void FixedUpdate()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+
+        if (horizontalAxis != 0)
+        {
+            submarineRb.AddForce(Vector3.right * (horizontalAxis * Time.deltaTime), ForceMode.Impulse);
+            submarineRb.AddForce(Vector3.up * (Input.GetAxis("Vertical") * Time.deltaTime), ForceMode.Impulse);
+            RotateToMovingDirection();
+        }
+        
+        ReduceVelocity();
+    }
+
+    private void RotateToMovingDirection()
+    {
+        if (horizontalAxis > 0f)
+        {
+            submarineRb.DORotate(rightRotation, 1f, RotateMode.Fast);
+        }
+        else if (horizontalAxis < 0f)
+        {
+            submarineRb.DORotate(leftRotation, 1f, RotateMode.Fast);
+        }
+        
+        propellor.transform.Rotate(Vector3.forward * (Time.deltaTime * propellorRotationSpeed * Mathf.Abs(horizontalAxis))
+            ,Space.Self);
+    }
+    
+    private void ReduceVelocity()
+    {
+        submarineRb.velocity = new Vector3(
+            submarineRb.velocity.x * 0.99f,
+            submarineRb.velocity.y * 0.96f,
+            submarineRb.velocity.z);
+    }
+}
